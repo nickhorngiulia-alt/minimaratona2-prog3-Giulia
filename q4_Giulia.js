e0a667d698b0bb05d8ed184c66946efa5aa7469c
nickhorngiulia-alt <nickhorngiulia@gmail.com>
2026-06-18 17:17:17 -0300

    Questão 4 finalizada

--- q4_Giulia.js
+++ q4_Giulia.js
@@ -2,16 +2,25 @@ function solucao(){
     let texto = document.querySelector("#input").value;
     let vetor = [];
     let contador = 1;
-    for (let i = 0; i < texto.length; i++){
-        let j = i + 1;
+    for (let i = 1; i < texto.length; i++){
+        let j = i - 1;
         if (texto[i] == texto[j]){
             contador++;
-        } else if (contador == 1){
-            vetor.push(texto[i]);
         } else {
-            vetor.push(texto[i] + contador);
+            if (contador == 1){
+                vetor.push(texto[j]);
+            } else {
+                vetor.push(texto[j] + contador);
+            }
+            contador = 1;
         }
     }
-document.querySelector("#saida").innerHTML = vetor.join();
+    let ultimoIndice = texto.length - 1;
+    if (contador == 1) {
+        vetor.push(texto[ultimoIndice]);
+    } else {
+        vetor.push(texto[ultimoIndice] + contador);
+    }
+document.querySelector("#saida").innerHTML = vetor.join("");
 }
 document.querySelector("#executar").addEventListener("click", solucao);
