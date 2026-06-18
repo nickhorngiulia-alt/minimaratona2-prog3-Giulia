72743af4f677e5e8af7eb0cf1ca7463a4fed31ed
nickhorngiulia-alt <nickhorngiulia@gmail.com>
2026-06-18 13:09:51 -0300

    Questão 2 finalizada

--- q2_Giulia.js
+++ q2_Giulia.js
@@ -1,21 +1,22 @@
-function verificar(){
-    let parenteses = document.querySelector("parenteses").value.split();
-    let mensagem = "";
-    for (let i = 0; i < parenteses.length; i++){
-        let contador = 0;
-        if (parenteses[i] == "("){
+function verificar() {
+    let parenteses = document.querySelector("input").value.split("");
+    let contador = 0;
+    let mensagem = "Válido";
+
+    for (let i = 0; i < parenteses.length; i++) {
+        if (parenteses[i] == "(") {
             contador++;
-            for (let j = 1; j < parenteses.length; j++){
-                if (parenteses[i] == ")"){
-                    contador--;
-                    break;
-                } else if (parenteses[i] == ")") {
-                    mensagem = "Inválido";
-                    } else {
-                        mensagem = "Válido"; //seguindo o exemplo, vazio = válido;
-                    }
-            }
+        } else if (parenteses[i] == ")") {
+            contador--;
+        }
+            if (contador < 0) {
+            mensagem = "Inválido";
+            break;
         }
     }
+    if (contador != 0) {
+        mensagem = "Inválido";
+    }
+    document.querySelector("#saida").innerHTML = mensagem;
 }
 document.querySelector("#executar").addEventListener("click", verificar);
