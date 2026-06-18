d0371d2d040b581fa4272baa9c58ce3194cfc5d4
nickhorngiulia-alt <nickhorngiulia@gmail.com>
2026-06-18 13:35:04 -0300

    Início da Questão 3

---
+++ q3_Giulia.js
@@ -0,0 +1,28 @@
+function verificar(){
+    let palavra1 = document.querySelector("#input1").value.toUpperCase().split("");
+    let palavra2 = document.querySelector("#input2").value.toUpperCase().split("");
+    let sim = "São anagramas";
+    let nao = "Não são anagramas";
+    let mensagem = "";
+    let contador = palavra1.length;
+    if (palavra1.length !== palavra2.length){
+        mensagem = nao;
+    } else {
+        for (let i = 0; i < palavra1.length; i++){
+            let letra = palavra1[i];
+            for (let j = 0; j < palavra2.length; j++){
+                if (letra = palavra2[j]){
+                    contador--;
+                }
+            }
+        }
+        if (contador !== 0){
+            mensagem = nao;
+        } else {
+            mensagem = sim;
+        }
+    }
+document.querySelector("#saida").innerHTML = mensagem;
+    
+}
+document.querySelector("#executar").addEventListener("click", verificar);
